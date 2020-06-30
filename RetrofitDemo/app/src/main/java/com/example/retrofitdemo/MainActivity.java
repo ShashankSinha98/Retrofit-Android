2455f701ca6fe6748c911dc7a30c934941232ea6
Shashank Sinha <34626597+ShashankSinha98@users.noreply.github.com>
2020-06-30 22:38:24 +0530

    Added code

--- RetrofitDemo/app/src/main/java/com/example/retrofitdemo/MainActivity.java
+++ RetrofitDemo/app/src/main/java/com/example/retrofitdemo/MainActivity.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -27,7 +29,8 @@ public class MainActivity extends AppCompatActivity {
     // Connverters are used to convert response from web service to Java Objects
 
     // JSON - [] - JSON Array, {} - JSON object - single java object
-    
+
+    private JsonPlaceholderApi jsonPlaceholderApi;
 
     private TextView textView;
 
@@ -43,11 +46,67 @@ public class MainActivity extends AppCompatActivity {
                 .addConverterFactory(GsonConverterFactory.create()) // mentioning how we want to parse the response
                 .build();
 
-        JsonPlaceholderApi jsonPlaceholderApi = retrofit.create(JsonPlaceholderApi.class); // retrofit will implement the get method in interface
+        jsonPlaceholderApi = retrofit.create(JsonPlaceholderApi.class); // retrofit will implement the get method in interface
 
-        Call<List<Post>> call = jsonPlaceholderApi.getPost();
+        //getPosts();
+        getComments();
+    }
 
-         // call.execute() - run on main thread - freeze app
+    private void getComments() {
+
+        //Call<List<Comment>> call = jsonPlaceholderApi.getComments(2);
+        //Call<List<Comment>> call = jsonPlaceholderApi.getComments("posts/1/comments");
+        // instead of just url, we can pass complete url in it..
+        Call<List<Comment>> call = jsonPlaceholderApi
+                .getComments("https://jsonplaceholder.typicode.com/posts/1/comments");// replace the base url passed in retrofit instance
+
+        call.enqueue(new Callback<List<Comment>>() {
+            @Override
+            public void onResponse(Call<List<Comment>> call, Response<List<Comment>> response) {
+                if(!response.isSuccessful()) { // HTTP code is btw 200 and 300, 404- error
+
+                    textView.setText("Code: "+response.code());
+                    return; // actual data is null from this response, if we try to do any operations on it, we'll get NullPointer Exception
+                }
+
+                List<Comment> comments = response.body();
+
+                for(Comment comment : comments){
+                    String content = "";
+
+                    content += "POST ID: "+comment.getPostId()+"\n"+
+                            "ID: "+comment.getId()+"\n"+
+                            "Name: "+comment.getName()+"\n"+
+                            "EMAIL: "+comment.getEmail()+"\n"+
+                    "TEXT: "+comment.getText()+"\n\n";
+
+                    textView.append(content);
+                }
+
+
+            }
+
+            @Override
+            public void onFailure(Call<List<Comment>> call, Throwable t) {
+                textView.setText(t.getMessage());
+            }
+        });
+
+    }
+
+    private void getPosts() {
+
+        //Call<List<Post>> call = jsonPlaceholderApi.getPostUsingMultipleQueries(null,"id","desc");
+        //Call<List<Post>> call = jsonPlaceholderApi.getPostForMultipleUserIds(new Integer[]{1,3});
+
+        Map<String, String> parameters = new HashMap<>();// Map is just interface like list, so cant initialize it as = new Map<>();
+        parameters.put("userId","1");// HashMap can take one key at a time, so we can't pass multiple userIds/ values for a single field that we want (downside of @QueryMap)
+        parameters.put("_sort","id");
+        parameters.put("_order","desc");
+
+        Call<List<Post>> call = jsonPlaceholderApi.getPostsWithDynamicQuery(parameters);
+
+        // call.execute() - run on main thread - freeze app
         // so we use call.enqueue
 
         call.enqueue(new Callback<List<Post>>() {
